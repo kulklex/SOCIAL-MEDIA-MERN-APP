16517dfc5730f87ad601fd2042106c51b8db7ce2
Kulklex <kulklex@gmail.com>
2022-07-16 00:46:36 +0100

    Google login API (With bugs) while logout feature fully implemented

--- frontEnd/src/components/Auth/Auth.jsx
+++ frontEnd/src/components/Auth/Auth.jsx
@@ -4,30 +4,53 @@ import {
   Container,
   Grid,
   Paper,
-  TextField,
   Typography,
 } from "@material-ui/core";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import useStyles from "./styles";
 import LockOutLinedIcon from "@material-ui/icons/LockOutlined";
 import Input from "./Input";
+import { GoogleLogin } from "react-google-login";
+import Icon from "./icon";
+import { useDispatch } from 'react-redux';
+import { useNavigate } from "react-router-dom";
 
 const Auth = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const classes = useStyles();
-  const [isSignUp,setIsSignUp] = useState(false)
-  const [showPassword, setShowPassword] = useState(false)
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const switchMode = () => {
     // To Toggle
-    setIsSignUp((prevIsSignUp) => !prevIsSignUp)
+    setIsSignUp((prevIsSignUp) => !prevIsSignUp);
     //Reset The Show & Hide Password to false, this will rest it and hide it
-    handleShowPassword(false)
-  }
+    handleShowPassword(false);
+  };
   const handleSubmit = () => {};
   const handleChange = () => {};
   const handleShowPassword = () => {
     //To Toggle Show & Hide Password
-    setShowPassword((prevShowPassword) => !prevShowPassword)
+    setShowPassword((prevShowPassword) => !prevShowPassword);
   };
+  const googleSuccess = async (res) => {
+    const result = res?.profileObj 
+    const token = res?.tokenId
+    try {
+      dispatch({type: 'AUTH', data: {result, token}})
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  };
+  const googleFailure = (err) => {
+    // window.alert(
+    //   console.error("Google Sign In was unsuccessful. Try Again Later")
+    // );
+    console.log(err)
+    console.error("Google Sign-In was unsuccessful. Try Again");
+  };
+
   return (
     <Container component="main" maxWidth="xs">
       <Paper className={classes.paper} elevation={3}>
@@ -56,29 +79,63 @@ const Auth = () => {
               </>
             )}
             <Input
-                  name="email"
-                  label="Email Address"
-                  handleChange={handleChange}
-                  type="email"
-                />
-                <Input
-                  name="password"
-                  label="Password"
-                  handleChange={handleChange}
-                  type={showPassword ? "text" : "password"}
-                  handleShowPassword={handleShowPassword}
-
-                />
-                {isSignUp && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password"/>}
-
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignUp && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
           </Grid>
-          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
             {isSignUp ? "Sign Up" : "Sign In"}
           </Button>
-          <Grid container justifyContent="flex-end">
+          {/* Google Login Setup */}
+          <GoogleLogin
+            clientId="1098171342336-qjncautuut11nrvba2etc0dfk2vgd294.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <Button
+                className={classes.googleButton}
+                color="primary"
+                fullWidth
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                variant="contained"
+                startIcon={<Icon/>}
+              >
+                Google Sign In
+              </Button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleFailure}
+            cookiePolicy="single_host_origin"
+          />
+         
+          <Grid container justifyContent="center">
             <Grid item>
               <Button onClick={switchMode}>
-                {isSignUp ? "Already have an account? Sign In": "Don't have an account? Sign Up"}
+                {isSignUp
+                  ? "Already have an account? Sign In"
+                  : "Don't have an account? Sign Up"}
               </Button>
             </Grid>
           </Grid>
--- frontEnd/src/components/Navbar/Navbar.jsx
+++ frontEnd/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 import memories from "../../images/memories.avif";
 import useStyles from "../../styles";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
 
 
 const Navbar = () => {
   const classes = useStyles();
-  const user = null
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile'))) //getting the credentials with name 'profile' from the local storage
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const logout = () => {
+    dispatch({type: 'LOGOUT'})
+    navigate('/')
+    setUser(null)
+  }
+
+  //using the use effect so we do not have to explicitly refresh the user state 
+  useEffect(()=> {
+    const token = user?.token
+
+    setUser(JSON.parse(localStorage.getItem('profile')))
+  }, [location])
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
@@ -29,7 +45,7 @@ const Navbar = () => {
       user ? (<div className={classes.profile}>
         <Avatar className={classes.purple} alt={user.result.name} src={user.result.imagUrl}> {user.result.name.charAt(0)} </Avatar>
         <Typography className={classes.username} variant="h6">{user.result.name}</Typography>
-        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+        <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
       </div>) : (
         <Link to='/auth'>
         <Button variant="contained" color="primary">Sign In</Button>
