2e3f920afe6263db8d9cb144e0b90c2bd081dc40
Kulklex <kulklex@gmail.com>
2022-07-16 00:37:59 +0100

    created google login reducer and combined

---
+++ frontEnd/src/redux/reducers/auth.js
@@ -0,0 +1,14 @@
+import {AUTH, LOGOUT} from "../../constants/actionTypes"
+
+const authReducer = (state = {authData: null}, action) => {
+    switch (action.type) {
+        case AUTH:
+            localStorage.setItem('profile', JSON.stringify({...action?.data})) //set the credentials the user inputs in our local storage and sending them for dispatch
+            return {...state, authData: action?.data};
+    
+        default:
+            return state;
+    }
+}
+
+export default authReducer
