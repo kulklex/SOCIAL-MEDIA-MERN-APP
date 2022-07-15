d018c9850457ac3574ef32b2ea180c15489851f4
Kulklex <kulklex@gmail.com>
2022-07-16 00:45:40 +0100

    LOGOUT reducer logic

--- frontEnd/src/redux/reducers/auth.js
+++ frontEnd/src/redux/reducers/auth.js
@@ -5,7 +5,9 @@ const authReducer = (state = {authData: null}, action) => {
         case AUTH:
             localStorage.setItem('profile', JSON.stringify({...action?.data})) //set the credentials the user inputs in our local storage and sending them for dispatch
             return {...state, authData: action?.data};
-    
+        case LOGOUT:
+            localStorage.clear()
+            return {...state, authData: null}
         default:
             return state;
     }
