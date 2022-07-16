f8eedc3bf3fab8212ce7e9ddfbad393bf640d086
Kulklex <kulklex@gmail.com>
2022-07-16 17:14:53 +0100

    added authorization to create, like, update and delete posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const {getPosts, createPosts, deletePost, updatePost, likePost} = require('../controllers/posts')
+const {verify} = require("../middleware/verify")
+
 
 router.get('/', getPosts)
-router.post('/', createPosts)
-router.patch('/:id', updatePost)
-router.delete('/:id', deletePost)
-router.patch('/:id/like', likePost)
+router.post('/', verify, createPosts)
+router.patch('/:id', verify, updatePost)
+router.delete('/:id', verify, deletePost)
+router.patch('/:id/like', verify, likePost)
 
 
 
