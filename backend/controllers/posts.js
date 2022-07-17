43b0c3e79cb1ddfec8d1a7e1e0d2c16c297c985b
Kulklex <kulklex@gmail.com>
2022-07-17 12:36:44 +0100

    Re-arranged the like post syntax and made sure a user can only like a post just once on server-side

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -52,12 +52,30 @@ const deletePost = async (req, res) => {
 
 const likePost = async (req, res) => {
     const {id} = req.params
+    if(!req.userId) return res.json({message: "Authorization Failed"}) //What's happening here is we're expecting a req.userId property from jwt in the verify middleware that'll be passed before this likePost controller, so we're making sure it is available else it should throw an error
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("The Id does not exist")
 
     const post = await PostMessage.findById(id)
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: ++post.likeCount}, {new: true})
 
-    res.status(200).json(updatePost)
+    const index = post.likes.findIndex((id) => id === String(req.userId)) 
+    //findIndex() returns the index number of the first element in an array to satisfy/pass the test written inside the parenthesis of the method
+    //In this case, we're looking for the first id in the likes array that is equal to the req.userId we're expecting from the middleware
+    //note that each like is going to have an id from a specific person, that's how we'll know who liked the specific post  
+
+    if(index === -1){
+        //Note that array index position -1 means the position doesn't exist
+        //so this will happen in this case only if the all the id we iterated through aren't equal to the req.userId(converted to string)
+        //once none of them is equal to req.userId we push req.userId and add it to the ids in the likes array
+        post.likes.push(req.userId)
+    } else {
+        //if there's a match for the id and req.userId
+        //we'll fetch all the likes except the current person's like i.e. we're filtering the likes array
+        post.likes = post.likes.filter(id => id !== String(req.userId))
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {new: true})
+
+    res.status(200).json(updatedPost)
 }
 
 module.exports = {getPosts, createPosts, deletePost, updatePost, likePost}
