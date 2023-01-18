4ff4fa6d7fae336654aee83ae7da5783aa5ffd49
kulklex <kulklex@gmail.com>
2023-01-18 18:01:46 +0100

    More styles for home page

--- frontEnd/src/styles.js
+++ frontEnd/src/styles.js
@@ -29,6 +29,22 @@ export default makeStyles((theme) => ({
         justifyContent: 'space-between',
         width: '400px'
       },
+      appBarSearch: {
+        borderRadius: 4,
+        marginBottom: '1rem',
+        display: 'flex',
+        padding: '16px',
+      },
+      pagination: {
+        borderRadius: 4,
+        marginTop: '1rem',
+        padding: '16px',
+      },
+      gridContainer: {
+        [theme.breakpoints.down('xs')]: {
+          flexDirection: 'column-reverse',
+        },
+      },
       userName: {
         display: 'flex',
         alignItems: 'center'
