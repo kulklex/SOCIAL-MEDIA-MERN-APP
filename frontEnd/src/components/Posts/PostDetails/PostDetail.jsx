236fdc914bc945e184dd196ec6ba69d556c457bd
kulklex <kulklex@gmail.com>
2023-01-18 10:35:46 +0100

    Created PostDetail file and style

---
+++ frontEnd/src/components/Posts/PostDetails/PostDetail.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import useStyles from './styles'
+
+
+const PostDetail = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            postdetai
+        </div>
+    );
+}
+
+export default PostDetail;
---
+++ frontEnd/src/components/Posts/PostDetails/styles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Styles = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Styles;
