a8eda50f6d3d7c2668166c986f3387d2c49832ec
kulklex <kulklex@gmail.com>
2023-01-23 15:38:09 +0100

    Circular Progress bar Implementation

--- frontEnd/src/components/Posts/Posts.jsx
+++ frontEnd/src/components/Posts/Posts.jsx
@@ -9,8 +9,9 @@ export default function Posts({setCurrentId}) {
   const posts = useSelector((state) => state.posts)
   const classes = useStyles()
   
-//Note that it is 'post.data' only when you have search query, you set the response for search controller as an object with 'data' as it's key
+  //Note that it is 'post.data' only when you have a search query, you set the response for getPostsBySearch controller(backend) as an object with 'data' as it's key
   if (posts.data){
+    if(posts.data.length === 0) return <CircularProgress/>
     return (<Grid className={classes.container} container alignItems="stretch" spacing={3}>
     {posts.data.map((post) => (
       <Grid key={post?._id} item xs={12} sm={6} md={6}>
@@ -19,6 +20,7 @@ export default function Posts({setCurrentId}) {
     ))}
   </Grid>)
    }  else {
+    if(posts.length === 0) return <CircularProgress/>
     return (<Grid className={classes.container} container alignItems="stretch" spacing={3}>
       {posts?.map((post) => (
         <Grid key={post?._id} item xs={12} sm={6} md={6}>
