b458727f37b6d07f936a8b2d282d87b31be61929
kulklex <kulklex@gmail.com>
2023-01-25 15:16:05 +0100

    Refactoring Likes Component and Impl for better UX

--- frontEnd/src/components/Posts/Likes.jsx
+++ frontEnd/src/components/Posts/Likes.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
 
-const Likes = ({ post, user }) => {
-  if (post.likes.length > 0) {
-    return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id)) 
+const Likes = ({ user, likes }) => {
+  if (likes.length > 0) {
+    return likes.find((like) => like === (user?.result?.googleId || user?.result?._id)) 
     ? (
       <>
         <ThumbUpAltIcon fontSize="small" />
         &nbsp;
-        {post.likes.length > 2
-          ? `You and ${post.likes.length - 1} others`
-          : `${post.likes.length} like${post.likes.length > 1 ? "s" : ""}`}
+        {likes.length > 2
+          ? `You and ${likes.length - 1} others`
+          : `${likes.length} like${likes.length > 1 ? "s" : ""}`}
       </>
     ) : (
       <>
         <ThumbUpAltOutlined fontSize="small" />
-        &nbsp;{post.likes.length} {post.likes.length === 1 ? "like" : "likes"}
+        &nbsp;{likes.length} {likes.length === 1 ? "like" : "likes"}
       </>
     );
   }
--- frontEnd/src/components/Posts/Post/Post.js
+++ frontEnd/src/components/Posts/Post/Post.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import useStyles from "./styles";
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -9,10 +9,28 @@ import { deletePost, likePost } from '../../../redux/actions/posts';
 import Likes from '../Likes';
 import { Link } from 'react-router-dom';
 
+
  function Post({post, setCurrentId}) {
+    const user = JSON.parse(localStorage.getItem('profile')) 
+    const userID = user?.result?.googleId || user?.result?._id
     const classes = useStyles()
     const dispatch = useDispatch()
-    const user = JSON.parse(localStorage.getItem('profile')) 
+
+    const [liked, setLiked] = useState(post?.likes)
+
+    const handleLike = () => {
+      dispatch(likePost(post._id))
+
+      // This condition means the user has already liked this post
+      // Therefore we will filter it out if the user clicks it again meaning the user is un-liking the post
+      // Else here will return the normal like count (spread operator) then add the user's like to the count
+      if(post.likes.find((like) => like === userID)) {
+        setLiked(post?.likes?.filter((id) => id !== userID))
+      } else {
+        setLiked([...post?.likes, userID])
+      }
+    }
+
     const deleteThePost = () => {
     if (window.confirm("Are you sure you want to delete this post")){
         return  dispatch(deletePost(post._id))
@@ -23,33 +41,31 @@ import { Link } from 'react-router-dom';
   
 
   return (
-   <Card className={classes.card}>
-    
+   <Card className={classes.card}> 
     <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title}/>
     <Link style={{textDecoration: 'none'}} to={`/posts/${post._id}`}  >
       <div className={classes.overlay}>
-      <Typography variant='h3'>{post.title}</Typography>
+        <Typography variant='h3'>{post.title}</Typography>
         <Typography variant='h6'>{post.name}</Typography>
         <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
-      </div>
-    
+      </div>   
 
       <div className={classes.details}>
           <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
-        </div>
-        <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
+      </div>
+      <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
       <CardContent>
           <Typography variant="body2" color="textSecondary" component="p">{post.message.split(' ').splice(0, 20).join(' ')}...</Typography>
       </CardContent>
     </Link>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color='primary'  disabled={!user?.result} onClick={() => {dispatch(likePost(post._id))} }>
-          <Likes post={post} user={user}/>
+        <Button size="small" color='primary'  disabled={!user?.result} onClick={handleLike}>
+          <Likes user={user} likes={liked}/>
         </Button>
         
       <div>
         {/* To make sure only user that created the post can edit it*/} 
-        {(user?.result?.googleId === post?.creator || user?.result?._id === post.creator) &&(
+        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) &&(
           <Button color='primary' size='small' onClick={(e) => {
               e.stopPropagation(); // stop propagation
               setCurrentId(post?._id)}}>
@@ -64,9 +80,6 @@ import { Link } from 'react-router-dom';
             Delete
           </Button>
         )}
-
-      
-
       </div>
        
       </CardActions>
