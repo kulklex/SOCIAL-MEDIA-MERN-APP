afac66eac1576f0d1e334ea02d459aaa88709401
kulklex <kulklex@gmail.com>
2023-01-30 11:37:56 +0100

    ChipInput for Form Tags

--- frontEnd/src/components/Form/Form.jsx
+++ frontEnd/src/components/Form/Form.jsx
@@ -2,9 +2,11 @@ import { Button, Paper, TextField, Typography } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
 import useStyles from "./styles";
 import FileBase from "react-file-base64";
+import ChipInput from 'material-ui-chip-input'
 import { useDispatch, useSelector } from "react-redux";
 import { createPosts, updatePost } from "../../redux/actions/posts";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function Form({currentId, setCurrentId}) {
   const classes = useStyles();
@@ -19,7 +21,7 @@ export default function Form({currentId, setCurrentId}) {
     selectedFile: " ",
   });
 
- 
+
 
   const clear = () => {
     setCurrentId(0)
@@ -35,20 +37,35 @@ export default function Form({currentId, setCurrentId}) {
 
   const handleSubmit = async (e) => {
     e.preventDefault(); //to avoid getting the refresh in the browser
-
-    if(currentId === 0) {
-      dispatch(createPosts({...postData, name: user?.result?.name }, navigate));
+   
+    const {title, message, tags} = postData
+    if (!title || !message || !tags) {
+      toast.error("Please fill all the fields")
+      return
     } else {
-      dispatch(updatePost(currentId, {...postData, name: user?.result?.name}, navigate));
-    } 
+      if(currentId === 0) {
+        dispatch(createPosts({...postData, name: user?.result?.name }, navigate));
+      } else {
+        dispatch(updatePost(currentId, {...postData, name: user?.result?.name}, navigate));
+      } 
     clear()
+    }
+
+  };
+
+  const handleAddChip = (tag) => {
+    setPostData({ ...postData, tags: [...postData.tags, tag] });
+  };
+
+  const handleDeleteChip = (chipToDelete) => {
+    setPostData({ ...postData, tags: postData.tags.filter((tag) => tag !== chipToDelete) });
   };
 
 
   if(!user?.result?.name){
     return (<Paper className={classes.paper} >
       <Typography variant="h6" align="center" >
-        Please Sign In to create your posts and like other people's posts
+        Please Sign In to create your posts and like other posts
       </Typography>
     </Paper>)
   }
@@ -67,6 +84,7 @@ export default function Form({currentId, setCurrentId}) {
             name="title"
             variant="outlined"
             label="Title"
+            required
             fullWidth
             value={postData.title}
             onChange={(e) => {
@@ -77,6 +95,7 @@ export default function Form({currentId, setCurrentId}) {
             name="message"
             variant="outlined"
             label="Message"
+            required
             fullWidth
             multiline
             minRows={4}
@@ -85,19 +104,32 @@ export default function Form({currentId, setCurrentId}) {
               setPostData({ ...postData, message: e.target.value });
             }}
           />
-          <TextField
+          <div style={{ padding: '5px 0', width: '94%' }}>
+            <ChipInput
             name="tags"
             variant="outlined"
             label="Tags"
             fullWidth
             value={postData.tags}
+            onAdd={(chip) => handleAddChip(chip)}
+            onDelete={(chip) => handleDeleteChip(chip)}
+          />
+          </div>
+          {/* <TextField
+            name="tags"
+            variant="outlined"
+            label="Tags"
+            fullWidth
+            required
+            value={postData.tags}
             onChange={(e) => {
               setPostData({ ...postData, tags: e.target.value.split(",") });
             }}
-          />
+          /> */}
           <div className={classes.fileInput}>
             <FileBase
               type="file"
+              src={postData.selectedFile}
               multiple={false}
               onDone={({ base64 }) =>
                 setPostData({ ...postData, selectedFile: base64 })
