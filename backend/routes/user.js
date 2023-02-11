8009edab94bb2073a4af141ccb4714f8301bcb24
Hassan Yusuff <kulklex@gmail.com>
2023-02-11 12:00:18 +0100

    Delete backend directory

--- backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const {signin, signup} = require('../controllers/user')
-
-router.post('/signin', signin)
-router.post('/signup', signup)
-
-
-
-module.exports = router
